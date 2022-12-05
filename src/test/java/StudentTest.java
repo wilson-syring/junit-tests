057643622f299de7359d8614f005bbefd37cddc3
Wilson Syring <wilson.syring@gmail.com>
2022-12-05 14:59:01 -0600

    configured the cohort test class

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,5 +1,34 @@
 import java.util.ArrayList;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class StudentTest {
-
+@Test
+    public void testCreate(){
+    Student one = new Student(1,"one");
+    Student bob = null;
+    assertNull(bob);
+    assertNotNull(one);
+}
+@Test
+    public void testFields(){
+    Student one = new Student(1,"one");
+    assertSame(1,one.getId());
+    assertSame("one",one.getName());
+    assertSame(0, one.getGrades().size());
+}
+@Test
+    public void testAdd(){
+    Student one = new Student(1,"one");
+    one.addGrade(90);
+    assertSame(90,one.getGrades().get(0));
+    one.addGrade(75);
+    assertSame(75,one.getGrades().get(1));
+}
+@Test
+    public void testAverage(){
+    Student one = new Student(1,"one");
+    one.addGrade(90);
+    one.addGrade(75);
+    assertEquals(82.5,one.getGradeAverage(),0);
+}
 }
