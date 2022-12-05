6f2afa0d3c816241e2bc04cffd43038899540f89
Wilson Syring <wilson.syring@gmail.com>
2022-12-05 14:59:48 -0600

    configured the student test class

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -4,21 +4,21 @@ import static org.junit.Assert.*;
 public class StudentTest {
 @Test
     public void testCreate(){
-    Student one = new Student(1L,"one");
+    Student one = new Student(1,"one");
     Student bob = null;
     assertNull(bob);
     assertNotNull(one);
 }
 @Test
     public void testFields(){
-    Student one = new Student(1L,"one");
+    Student one = new Student(1,"one");
     assertSame(1l,one.getId());
     assertSame("one",one.getName());
     assertSame(0, one.getGrades().size());
 }
 @Test
     public void testAdd(){
-    Student one = new Student(1L,"one");
+    Student one = new Student(1,"one");
     one.addGrade(90);
     assertSame(90,one.getGrades().get(0));
     one.addGrade(75);
@@ -26,7 +26,7 @@ public class StudentTest {
 }
 @Test
     public void testAverage(){
-    Student one = new Student(1L,"one");
+    Student one = new Student(1,"one");
     one.addGrade(90);
     one.addGrade(75);
     assertEquals(82.5,one.getGradeAverage(),0);
